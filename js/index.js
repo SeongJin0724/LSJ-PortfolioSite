ea4b3a11d8c84e9ee47f6aa223dbb5a3b449618d
SeongJin0724 <thls702464@gmail.com>
2022-06-22 01:19:02 +0900

    포트폴리오 80% 완료

--- js/index.js
+++ js/index.js
@@ -22,8 +22,9 @@ function hide() {
 }
 
 const topBtn = document.querySelector(".moveTopBtn");
-
+const about = document.querySelector(".moveBtn");
 topBtn.addEventListener("click", scrollTopHandler);
+about.addEventListener("click", scrollAboutHandler);
 
 function scrollTopHandler(item) {
   const tabTarget = item.currentTarget;
@@ -33,4 +34,41 @@ function scrollTopHandler(item) {
     target.classList.remove("on");
   });
   info_title.classList.remove("hide");
+}
+function scrollAboutHandler() {
+  window.scrollTo({ top: 5300, behavior: "smooth" });
+}
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY >= 5280) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+  console.log(window.scrollY);
+});
+
+const skill_content = document.querySelectorAll(".skill_content");
+const skill_content_text = document.querySelectorAll(".skill_content_text");
+
+skill_content.forEach((item) => {
+  item.addEventListener("mouseover", skilTabHandler);
+});
+skill_content.forEach((item) => {
+  item.addEventListener("mouseleave", skilRemoveHandler);
+});
+function skilTabHandler(item) {
+  const tabTarget = item.currentTarget;
+  const target = tabTarget.dataset.tab;
+  skill_content_text.forEach((target) => {
+    target.classList.remove("show");
+  });
+  document.querySelector("#" + target).classList.add("show");
+}
+function skilRemoveHandler(item) {
+  const tabTarget = item.currentTarget;
+  const target = tabTarget.dataset.tab;
+  skill_content_text.forEach((target) => {
+    target.classList.remove("show");
+  });
 }
